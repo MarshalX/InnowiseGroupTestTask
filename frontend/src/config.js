1d0fa1ba7127e96482dc75def112524953f862f3
Il'ya <marshal@xaker.ru>
2019-08-21 15:09:39 +0300

    Исправлен env для react'a

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
-let api_server = process.env.API_HOST || 'localhost';
-let api_port = process.env.API_PORT || 8000;
+let api_server = process.env.REACT_APP_API_HOST || 'localhost';
+let api_port = process.env.REACT_APP_API_PORT || 8000;
 
 const api_path = api_server + ":" + api_port;
 
