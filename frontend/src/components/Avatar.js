3c80b8663c0262c2c22f7aa84c30a93fab178abb
Il'ya <marshal@xaker.ru>
2019-08-22 16:09:48 +0300

    Добавлено отображение аватарки.

---
+++ frontend/src/components/Avatar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {Container, Row, Col, Image} from 'react-bootstrap'
+
+
+class Avatar extends React.Component {
+    render() {
+        const url = this.props.data[1];
+
+        return <Container>
+            <Row>
+                <Col xs={6} md={4}>
+                    <Image height="50px" src={url} rounded/>
+                    {this.props.children}
+                </Col>
+            </Row>
+        </Container>
+    }
+}
+
+export default Avatar;
