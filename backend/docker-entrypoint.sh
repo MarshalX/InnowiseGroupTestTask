7ed6a1c3a0d2cb0bf6f13d90ffa3ec5074ee5246
Il'ya <marshal@xaker.ru>
2019-08-23 11:24:01 +0300

    Коллект статики на проде.

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd api
+mkdir static
+python manage.py collectstatic
 python manage.py migrate
-python manage.py loaddata library/fixtures/auth_user_initial_data
-python manage.py loaddata library/fixtures/library_initial_data
 python manage.py runserver 0.0.0.0:8000
