dadc0bd5d8f321bb08fa022e5bf34e4976625c3c
Il'ya <marshal@xaker.ru>
2019-08-26 16:28:21 +0300

    Установлен Django Debug Toolbar

--- backend/api/api/urls.py
+++ backend/api/api/urls.py
@@ -18,3 +18,7 @@ urlpatterns += [
     url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
